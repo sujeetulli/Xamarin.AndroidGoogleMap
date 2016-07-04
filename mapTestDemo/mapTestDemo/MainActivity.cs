4fd62f7b41437f4ef4aa49ca6a65f092ef5b6f96
MCSINL030 <mcsinl030@MCSINH-MAC-PROs-MacBook-Pro-2.local>
2016-07-05 01:38:54 +0530

    Project files added

---
+++ mapTestDemo/mapTestDemo/MainActivity.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Common;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Locations;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mapTestDemo
+{
+	[Activity (Label = "mapTestDemo", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity,IOnMapReadyCallback, Android.Gms.Maps.GoogleMap.IInfoWindowAdapter
+	{
+		int count = 1;
+		public static readonly int InstallGooglePlayServicesId = 1000;
+		private bool _isGooglePlayServicesInstalled;
+
+		private MapFragment _mapFragment;
+
+		public GoogleMap map { get; private set; }
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			if (isGPSEnabled ()) {
+
+				_isGooglePlayServicesInstalled = TestIfGooglePlayServicesIsInstalled ();
+
+				if (_isGooglePlayServicesInstalled) {
+					//since we use a test key the map won't work on all test devices. Only at Kapil his device.
+					//This will work when we move the android version to the real parse.com db.
+					InitMapFragment ();
+				} else {
+					AlertDialog.Builder dialog = new AlertDialog.Builder (this, AlertDialog.ThemeHoloLight);
+					dialog.SetTitle ("map Error");
+					dialog.SetMessage ("Map error message");
+					dialog.SetCancelable (false);
+					dialog.SetPositiveButton ("ok", delegate {
+						return;
+					});
+					dialog.SetNegativeButton ("cancel", delegate {
+						return;
+					});
+					dialog.Show ();
+				}
+			}
+		}
+
+		public bool isGPSEnabled(){
+			LocationManager LocMgr = Android.App.Application.Context.GetSystemService (Context.LocationService) as LocationManager;
+			bool enabled = LocMgr.IsProviderEnabled (LocationManager.GpsProvider);
+
+			if (!enabled) {
+				AlertDialog.Builder dialog = new AlertDialog.Builder(this, AlertDialog.ThemeHoloLight);
+				dialog.SetTitle ("GPS Alert");
+				dialog.SetMessage ("Your GPS is OFF Mode");
+				dialog.SetCancelable (false);
+				dialog.SetPositiveButton ("GOTO GPS Settting", delegate {
+					Intent intent = new Intent(Android.Provider.Settings.ActionLocationSourceSettings);
+					StartActivity(intent);
+				});
+				dialog.SetNegativeButton ("Cancel", delegate { return; });
+				dialog.Show ();
+			}
+
+			return enabled;
+		}
+
+		private bool TestIfGooglePlayServicesIsInstalled ()
+		{
+			try {
+				int queryResult = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable (this);
+				if (queryResult == ConnectionResult.Success) {
+					Console.WriteLine ("Google Play Services is installed on this device.");
+					return true;
+				}
+
+				if (GoogleApiAvailability.Instance.IsUserResolvableError (queryResult)) {
+					string errorString = GoogleApiAvailability.Instance.GetErrorString (queryResult);
+					Console.WriteLine ("There is a problem with Google Play Services on this device: {0} - {1}", queryResult, errorString);
+					Dialog errorDialog = GoogleApiAvailability.Instance.GetErrorDialog (this, queryResult, InstallGooglePlayServicesId);
+					errorDialog.Show ();
+				}
+			} catch (Exception ex) {
+				Console.WriteLine ("Test for google play services {0}",ex.ToString());
+			}
+			return false;
+		}
+
+		private void InitMapFragment()
+		{
+			_mapFragment = FragmentManager.FindFragmentByTag("map") as MapFragment;
+
+			if (_mapFragment == null)
+			{
+				GoogleMapOptions mapOptions = new GoogleMapOptions()
+					.InvokeMapType(GoogleMap.MapTypeNormal)
+					.InvokeZoomControlsEnabled(true).InvokeScrollGesturesEnabled(true)
+					.InvokeCompassEnabled(true);
+
+				FragmentTransaction fragTx = FragmentManager.BeginTransaction();
+				_mapFragment = MapFragment.NewInstance(mapOptions);
+				fragTx.Add(Resource.Id.map, _mapFragment, "map");
+				fragTx.Commit();
+			}
+
+			_mapFragment.GetMapAsync (this);
+
+		}
+
+		public void OnMapReady (GoogleMap googleMap)
+		{
+			map = googleMap;
+			map.MyLocationEnabled = true;
+			map.MyLocationChange += Map_MyLocationChange;
+		}
+
+		void Map_MyLocationChange (object sender, GoogleMap.MyLocationChangeEventArgs e)
+		{
+			map.MyLocationChange -= Map_MyLocationChange;
+			LatLng latLong =new LatLng(e.Location.Latitude,e.Location.Longitude);
+			CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+
+			builder.Target(latLong);
+			builder.Zoom(10);
+			builder.Bearing(155);
+			builder.Tilt(65);
+			CameraPosition cameraPosition = builder.Build();
+			MarkerOptions markerOpt = new MarkerOptions ();
+			markerOpt.SetPosition (latLong);
+			markerOpt.SetIcon (BitmapDescriptorFactory.FromResource (Resource.Drawable.Map_icon));
+			map.AddMarker (markerOpt);
+			map.SetInfoWindowAdapter (this);
+			map.AnimateCamera(CameraUpdateFactory.NewCameraPosition(cameraPosition));
+		}
+
+		public View GetInfoContents (Marker marker){
+			var inflater = Application.Context.GetSystemService(Context.LayoutInflaterService) as LayoutInflater;
+
+			View v = inflater.Inflate(Resource.Layout.MapInfoView, null);
+
+			Location locationA = new Location("point A");
+			locationA.Latitude = -36.722195;
+			locationA.Longitude = 174.706166;
+
+			Location locationB = new Location("point B");
+			locationB.Latitude = marker.Position.Latitude;
+			locationB.Longitude = marker.Position.Longitude;
+
+			float distance = locationA.DistanceTo(locationB) / 1000;
+
+			TextView title = (TextView) v.FindViewById(Resource.Id.Phone);
+
+			DateTime now = DateTime.Now.ToLocalTime();
+			if (DateTime.Now.IsDaylightSavingTime () == true) {
+				now = now.AddHours (1);
+			}
+			var utc = DateTime.UtcNow;
+
+			title.Text = "lat:" + marker.Position.Latitude + " long:" + marker.Position.Longitude;
+
+			TextView description = (TextView) v.FindViewById(Resource.Id.Web);
+			description.Text = "Local Time:"+now+ "\nUTC Time:"+utc+"\nDistance:" + distance;
+
+			return v;	
+		}
+
+		public View GetInfoWindow (Marker marker){
+			return null;
+		}
+	}
+}
+	
---
+++ mapTestDemo/mapTestDemo/obj/Debug/android/src/md54dd8c71fecb093240a8bdde31985458b/MainActivity.java
@@ -0,0 +1,75 @@
+package md54dd8c71fecb093240a8bdde31985458b;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer,
+		com.google.android.gms.maps.OnMapReadyCallback,
+		com.google.android.gms.maps.GoogleMap.InfoWindowAdapter
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onMapReady:(Lcom/google/android/gms/maps/GoogleMap;)V:GetOnMapReady_Lcom_google_android_gms_maps_GoogleMap_Handler:Android.Gms.Maps.IOnMapReadyCallbackInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"n_getInfoContents:(Lcom/google/android/gms/maps/model/Marker;)Landroid/view/View;:GetGetInfoContents_Lcom_google_android_gms_maps_model_Marker_Handler:Android.Gms.Maps.GoogleMap/IInfoWindowAdapterInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"n_getInfoWindow:(Lcom/google/android/gms/maps/model/Marker;)Landroid/view/View;:GetGetInfoWindow_Lcom_google_android_gms_maps_model_Marker_Handler:Android.Gms.Maps.GoogleMap/IInfoWindowAdapterInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"";
+		mono.android.Runtime.register ("mapTestDemo.MainActivity, mapTestDemo, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("mapTestDemo.MainActivity, mapTestDemo, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onMapReady (com.google.android.gms.maps.GoogleMap p0)
+	{
+		n_onMapReady (p0);
+	}
+
+	private native void n_onMapReady (com.google.android.gms.maps.GoogleMap p0);
+
+
+	public android.view.View getInfoContents (com.google.android.gms.maps.model.Marker p0)
+	{
+		return n_getInfoContents (p0);
+	}
+
+	private native android.view.View n_getInfoContents (com.google.android.gms.maps.model.Marker p0);
+
+
+	public android.view.View getInfoWindow (com.google.android.gms.maps.model.Marker p0)
+	{
+		return n_getInfoWindow (p0);
+	}
+
+	private native android.view.View n_getInfoWindow (com.google.android.gms.maps.model.Marker p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
